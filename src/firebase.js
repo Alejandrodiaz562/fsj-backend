f78c7c61c653417bbb2efff1cfa5b0e40d53eca5
Alejandrodiaz562 <111014821+Alejandrodiaz562@users.noreply.github.com>
2025-03-28 07:37:50 -0500

    fixing errors

--- src/firebase.js
+++ src/firebase.js
@@ -1,5 +1,5 @@
 import admin from "firebase-admin";
-import { readFile } from "fs/promises";
+/*import { readFile } from "fs/promises";
 
 const serviceAccount = JSON.parse(
     await readFile(new URL("../firebase2.json", import.meta.url))
@@ -7,6 +7,14 @@ const serviceAccount = JSON.parse(
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
+});
+
+*/
+
+import dotenv from "dotenv";
+dotenv.config(); // Cargar variables de entorno desde .env
+admin.initializeApp({
+  credential: admin.credential.applicationDefault(), // Usa GOOGLE_APPLICATION_CREDENTIALS
 });
 
 const db = admin.firestore();
