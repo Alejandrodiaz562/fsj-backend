6b5f8a4e808b255c465ffa96bc85b9f446665aba
Alejandrodiaz562 <111014821+Alejandrodiaz562@users.noreply.github.com>
2025-03-28 22:06:58 -0500

    fixing errors

--- src/firebase.js
+++ src/firebase.js
@@ -17,7 +17,8 @@ admin.initializeApp({
   credential: admin.credential.applicationDefault(), // Usa GOOGLE_APPLICATION_CREDENTIALS
 });*/
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+const serviceAccount = JSON.parse(process.env.GOOGLE_APPLICATION_CREDENTIALS);
+
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
