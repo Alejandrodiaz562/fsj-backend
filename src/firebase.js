7a526b81a324ca21c16c7b82cfc77836deb43a13
Alejandrodiaz562 <111014821+Alejandrodiaz562@users.noreply.github.com>
2025-03-28 21:36:37 -0500

    fixing errors

--- src/firebase.js
+++ src/firebase.js
@@ -11,10 +11,15 @@ admin.initializeApp({
 
 */
 
-import dotenv from "dotenv";
+/*import dotenv from "dotenv";
 dotenv.config(); // Cargar variables de entorno desde .env
 admin.initializeApp({
   credential: admin.credential.applicationDefault(), // Usa GOOGLE_APPLICATION_CREDENTIALS
+});*/
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
