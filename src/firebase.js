3a0d045d5b6ad86b658a011eb0dcd22ee3dbc0b4
Alejandrodiaz562 <111014821+Alejandrodiaz562@users.noreply.github.com>
2025-04-11 23:06:31 -0500

    fixing errors

--- src/firebase.js
+++ src/firebase.js
@@ -1,21 +1,39 @@
 import admin from "firebase-admin";
-import fs from "fs";
+//import fs from "fs";
 import "dotenv/config"; // Asegúrate de importar dotenv si usas un archivo .env
 
-const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+//const serviceAccountPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+const credentialsJSON = process.env.FIREBASE_CREDENTIALS_JSON;
 
-if (!serviceAccountPath) {
+/*if (!serviceAccountPath) {
   throw new Error("La variable de entorno GOOGLE_APPLICATION_CREDENTIALS no está definida.");
+}*/
+
+if (!credentialsJSON) {
+  throw new Error("La variable de entorno FIREBASE_CREDENTIALS_JSON no está definida.");
 }
-console.log("Ruta a credenciales:", serviceAccountPath);
-console.log("¿El archivo existe?", fs.existsSync(serviceAccountPath));
-const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
+
+//console.log("Ruta a credenciales:", serviceAccountPath);
+//console.log("¿El archivo existe?", fs.existsSync(serviceAccountPath));
+//const serviceAccount = JSON.parse(fs.readFileSync(serviceAccountPath, "utf-8"));
 
 
-admin.initializeApp({
+/*admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
 
-export default db;
+export default db;*/
+
+try {
+  const serviceAccount = JSON.parse(credentialsJSON);
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+  });
+  const db = admin.firestore();
+  export default db;
+} catch (error) {
+  console.error("Error al parsear las credenciales de Firebase:", error);
+  throw new Error("Error al configurar Firebase.");
+}
